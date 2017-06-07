0acac9aa3a0691aeb0a61cf96d68401bc938dd6b
Lord Vollkorn <bertozzi@i-ways.de>
2017-06-07 16:32:22 +0200

    tunings

---
+++ lvk/helpme.sh
@@ -0,0 +1,24 @@
+START_TIME=0;
+
+/Applications/XAMPP/bin/php bin/magento setup:upgrade;
+rm -R var/cache 2> /dev/null;
+rm -R var/di 2> /dev/null;
+rm -R var/generation 2> /dev/null;
+rm -R var/page_cache 2> /dev/null;
+rm -R var/view_preprocessed 2> /dev/null;
+
+rm -R pub/static/_requirejs 2> /dev/null;
+rm -R pub/static/adminhtml 2> /dev/null;
+rm -R pub/static/frontend 2> /dev/null;
+
+while getopts cd opt
+do
+   case $opt in
+       c) /Applications/XAMPP/bin/php bin/magento setup:di:compile;;
+       d) /Applications/XAMPP/bin/php bin/magento setup:static-content:deploy de_DE;;
+   esac
+done
+
+printf "\nhelping process took $SECONDS seconds\n\n";
+
+osascript -e 'display notification "May the PASTA be always with you!" with title "Lord Vollkorn says: DONE!"';
