f112bf9e588b8303af588d198ea1a2047c77ab0a
Lord Vollkorn <bertozzi@i-ways.de>
2017-06-07 09:25:05 +0200

    better resources naming

--- app/design/frontend/Vollkorn/pasta/registration.php
+++ app/design/frontend/Vollkorn/pasta/registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/LordVollkorn/pasta',
+    'frontend/Vollkorn/pasta',
     __DIR__
 );
