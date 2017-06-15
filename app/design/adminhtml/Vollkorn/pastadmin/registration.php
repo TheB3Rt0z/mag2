429f1305dba508d9e3dbdadd0c324353175addcf
Lord Vollkorn <bertozzi@i-ways.de>
2017-06-15 09:50:39 +0200

    updated backend theme -> extension

--- app/design/adminhtml/Vollkorn/pastadmin/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'adminhtml/Vollkorn/pastadmin',
-    __DIR__
-);
